a2cdef1d938b5824d43235b79f35c8c92d352fc4
chris24george <chris24george@gmail.com>
2022-10-19 12:08:55 -0700

    getting functions to talk to json server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,17 @@
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
 import { useContext } from 'react';
+import Spinner from './shared/Spinner';
 
 function FeedbackList() {
 
-  const {feedback} = useContext(FeedbackContext);
+  const {feedback, isLoading} = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet.</p>
   }
 
-  return (
+  return isLoading ? <Spinner /> : ( 
     <div className="feedback-list">
       {
         feedback.map((item) => (
@@ -21,7 +22,7 @@ function FeedbackList() {
         ))
       }
     </div>
-  )
+  );
 }
 
 
